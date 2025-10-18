164aa00e73ae6e37945404437718f1c7733198ba
yunjik <yunjikyee@gmail.com>
2025-10-19 00:56:12 +0900

    (#2) task2 commit 11651

---
+++ week1/yunjik/0002.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define max 100000
+struct point {			//점위치 받을 구조체
+	int x;
+	int y;
+};
+	
+int main(void) {											
+	struct point p[max];
+	int num;
+	scanf("%d", &num);	//점 몇개할지 입력
+	for (int f=0;f < num;f++) {
+		scanf("%d %d", &p[f].x, &p[f].y);
+	}	
+
+	for (int i = 0;i < num - 1;i++) {
+		for (int j = 0;j < num - i - 1;j++) {
+			if (p[j].y > p[j + 1].y|| (p[j].y==p[j+1].y&&p[j].x>p[j+1].x) ) {	//y값을 정렬시키면서 y값이 같을때 x값도 비교해서 정렬한다
+
+				struct point temp = p[j];		//struct point temp는 구조체 값 잠시 저장할 임시임(int temp의 구조체 버전)
+				p[j] = p[j + 1];
+				p[j + 1] = temp;
+
+			}
+		}
+	}
+
+	for (int ii = 0;ii < num;ii++) {
+		printf("\n%d %d", p[ii].x, p[ii].y);
+	}
+	
+	return 0;
+}
